a3fcb822239a6bb931dcd4ecfd5e6f08c9e9f611
userbq201 <yzdrf98@mail.ru>
2017-08-23 13:18:58 +0300

    policy

--- src/app/policy/users/readBlock.ts
+++ src/app/policy/users/readBlock.ts
@@ -1,4 +1,4 @@
-export default (args: any[]): Promise<any> => { 
+export default (args: any[]): Promise<boolean> => { 
   return new Promise((res: typeof Promise.resolve, rej: typeof Promise.reject) => { 
     res(true);
   });
